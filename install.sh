b2536bc24e0e3bb9654995797f1a4f05674157e9
727706910@qq.com <727706910@qq.com>
2018-04-19 16:53:11 +0800

    change code for install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/usr/bin/sh
+echo "
++------------------------------------------------------+
+|                                                      |
+|                      VimForCpp                       |
+|                                                      |
++------------------------------------------------------+
+安装开始!
+整个过程大概需要下载大约 200MB 数据. 流量党请慎入!
+"
 # 切换到 root 用户, 并且触发安装脚本
 install_user_home=$HOME
 
 function SwitchRoot() {
-  echo "尝试切换到 root 用户, 请输入 root 密码:"
-  su -c "curl -sLf https://gitee.com/HGtz2222/VimForCpp/raw/master/start.sh -o /tmp/start.sh && bash -x /tmp/start.sh $install_user_home"
+  echo "为了安装依赖程序, 需要使用 root 账户. 您的密码不会被上传."
+  echo "请输入 root 密码:"
+  su -c "curl -sLf https://gitee.com/HGtz2222/VimForCpp/raw/master/start.sh -o /tmp/start.sh && bash /tmp/start.sh $install_user_home"
 }
 
 # 切换到 root 用户
